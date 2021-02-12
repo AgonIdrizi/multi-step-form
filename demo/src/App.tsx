adcb47e2188c1eb6e69eb34a2c09f79ee7d3e94d
AgonIdrizi <agon.88@hotmail.com>
2021-02-12 16:04:51 +0100

    add lib/demo folders

---
+++ demo/src/App.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import FormContainer  from 'multi-step-form-react/lib/components';
+
+
+// TODO: finally make it work without the extra /lib path
+
+// import { List, Item } from 'mh-react-library/data';
+// import { SelectButton } from 'mh-react-library/select';
+
+const App = () => (
+  <div>
+    <FormContainer />
+  </div>
+);
+
+export default App;
--- src/components/FormContainer/Input/Input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Input.css'
-
-const Input = ({ field, form, ...props }) => {
-  return ( <input {...field} {...props}  />);
-}
-
-export default Input;
